e4d007abddca8467ddf33e9bd1fafb95c0c62b00
brunomgferreira <brunomgferreira17@gmail.com>
2024-04-15 12:55:09 +0100

    Created the UI of the inbox page

--- templates/header.tpl.php
+++ templates/header.tpl.php
@@ -30,6 +30,7 @@ require_once (__DIR__ . '/../utils/session.php');
     <link rel="stylesheet" href="../css/item.css">
     <link rel="stylesheet" href="../css/items.css">
     <link rel="stylesheet" href="../css/profile.css">
+    <link rel="stylesheet" href="../css/inbox.css">
   </head>
 
   <body>
