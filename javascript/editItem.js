55855b10fec3804ffb041a5d55c32be25543ca0e
brunomgferreira <brunomgferreira17@gmail.com>
2024-04-23 09:19:05 +0100

    Edit item description is done

--- javascript/editItem.js
+++ javascript/editItem.js
@@ -1,3 +1,53 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("edit-item-price")
+    .addEventListener("change", function () {
+      this.value = parseFloat(this.value).toFixed(2);
+    });
+
+  // TEXT AREA CODE (Description) - TODO: clean up code
+  const textarea = document.getElementById("edit-item-description");
+  // Add an event listener for change event
+  textarea.addEventListener("input", function () {
+    // Get the text inside the textarea
+    var text = textarea.value;
+    // Print the text
+    text = text.replace(/\n/g, "<br>");
+    text = DOMPurify.sanitize(text, { ALLOWED_TAGS: ["br"] });
+  });
+
+  const editItemPrice = document.getElementById("edit-item-price");
+
+  editItemPrice.addEventListener("input", function (event) {
+    // Get the input value
+    var inputValue = event.target.value;
+
+    // Remove any non-digit or non-decimal characters except the first dot
+    inputValue = inputValue.replace(/[^\d.]/g, "");
+
+    // Ensure there's only one dot
+    var dotIndex = inputValue.indexOf(".");
+    if (dotIndex !== -1) {
+      inputValue =
+        inputValue.substr(0, dotIndex + 1) +
+        inputValue.substr(dotIndex + 1).replace(/\./g, "");
+    }
+
+    // Remove leading zero if followed by another digit
+    inputValue = inputValue.replace(/^0+(?=\d)/, "");
+
+    // Ensure it's a positive number with up to two decimal places
+    var decimalRegex = /^\d*\.?\d{0,2}$/;
+    if (!decimalRegex.test(inputValue)) {
+      // If not a valid number, set value to empty
+      inputValue = "0";
+    }
+
+    // Update the input value
+    editItemPrice.value = inputValue;
+  });
+});
+
 function previewImage(event) {
   const file = event.target.files[0];
   const reader = new FileReader();
